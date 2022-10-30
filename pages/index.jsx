24f4c7de6d720c5a33611254bd5f4d28a9466abd
Adithya <jhadith88@gmail.com>
2022-10-30 17:09:53 +0530

    added header

---
+++ pages/index.jsx
@@ -0,0 +1,21 @@
+// import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+
+const Home = () => {
+  return (
+    <div className="">
+      <Head>
+        <title>APPLE REDESIGN</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     
+    <Header/>
+
+    {/* from 1:13:32 */}
+    </div>
+  )
+}
+
+export default Home
